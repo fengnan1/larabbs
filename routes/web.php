db35796b92857a8fcdc0e4ea2eebd577e441feba
fengnan1 <811264657@qq.com>
2021-05-03 17:35:52 +0800

    代码生成器-生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,4 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //用户相关
 Route::resource('users', 'Home\UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
