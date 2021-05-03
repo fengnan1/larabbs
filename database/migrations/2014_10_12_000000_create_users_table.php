5db67fd704cf074452447958b07783cbd414fbb6
fengnan1 <811264657@qq.com>
2021-05-03 14:41:25 +0800

    编辑个人资料

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable()->comment('头像');
+            $table->string('introduction')->nullable()->comment('个人简介');
             $table->rememberToken();
             $table->timestamps();
         });
