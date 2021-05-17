dce3efe19f625e898349292817246dcbe5c986d2
fengnan1 <811264657@qq.com>
2021-05-17 10:27:51 +0000

    发送短信验证码

--- config/app.php
+++ config/app.php
@@ -106,7 +106,7 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
+    'faker_locale' => 'zh_CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -175,6 +175,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        App\Providers\EasySmsServiceProvider::class,
 
     ],
 
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->phoneNumber,
         'remember_token' => Str::random(10),
         'email_verified_at' => now(),
         'password' => bcrypt('123456'), // secret
