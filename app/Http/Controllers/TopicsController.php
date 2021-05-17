57224d1fbdf8cfa3ba70b6e624211a5ce5b11c6c
vagrant <vagrant@homestead>
2021-05-17 02:58:28 +0000

    侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,7 +9,7 @@ use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use Auth;
 use App\Handlers\ImageUploadHandler;
-
+use App\Models\Link;
 use App\Models\User;
 class TopicsController extends Controller
 {
@@ -18,14 +18,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic, User $user)
+    public function index(Request $request, Topic $topic, User $user,Link $link)
     {
         $topics = $topic->withOrder($request->order)
             ->with('user', 'category')  // 预加载防止 N+1 问题
             ->paginate(20);
         $active_users = $user->getActiveUsers();
-        // dd($active_users);
-        return view('topics.index', compact('topics', 'active_users'));
+        $links = $link->getAllCached();
+        return view('topics.index', compact('topics', 'active_users','links'));
     }
 
     public function show(Request $request,Topic $topic)
