b0e26b16b2cc9b105b1adf8ea20951ff2fb1903d
fengnan1 <811264657@qq.com>
2021-05-18 06:35:39 +0000

    s

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,30 +17,15 @@ class User extends Authenticatable implements  MustVerifyEmailContract
         notify as protected laravelNotify;
     }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'phone', 'email', 'password', 'introduction', 'avatar',
         'weixin_openid', 'weixin_unionid'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
